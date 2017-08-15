ed80e89ce6f1cbc6dd2bc31813e8df064714b43e
KaiserChan <chankaiserkk@gmail.com>
2017-08-15 15:21:51 -0400

    Created part_1_bankaccount.rb - Completed exercise

---
+++ part_1_bankaccount.rb
@@ -0,0 +1,71 @@
+class BankAccount
+
+#class variables below ----------
+  @@interest_rate = 0.01
+  @@accounts = []
+
+#class methods below ----------
+  def self.create
+    new_accounts = BankAccount.new
+    @@accounts << new_accounts
+    return new_accounts
+  end
+
+
+  def self.total_funds
+    total_funds = 0
+    @@accounts.each do |entry|
+      total_funds += entry.balance
+    end
+    return total_funds
+  end
+
+  def self.interest_time
+    @@accounts.each do |entry|
+      interest = entry.balance * (@@interest_rate)
+      entry.deposit(interest)
+    end
+  end
+
+#instance variables below ----------
+  def initialize
+    @balance = 0
+  end
+
+#instance methods below ----------
+  def balance
+    @balance
+  end
+
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def withdrawal(amount)
+    @balance -= amount
+  end
+
+end
+
+
+my_account = BankAccount.create
+your_account = BankAccount.create
+
+puts my_account.balance
+puts BankAccount.total_funds
+
+my_account.deposit(10000)
+your_account.deposit(12500)
+
+puts my_account.balance
+puts your_account.balance
+puts BankAccount.total_funds
+
+BankAccount.interest_time
+puts my_account.balance
+puts your_account.balance
+puts BankAccount.total_funds
+
+my_account.withdrawal(2500)
+puts my_account.balance
+puts BankAccount.total_funds
